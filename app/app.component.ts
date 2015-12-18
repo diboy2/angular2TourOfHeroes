6175c6c43928cc7d7aa069d528e579e2469fb8dd
Adrian Cagaanan <adrian.cagaanan@cerner.com>
2015-12-18 12:17:45 -0600

    Added quickstart files

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by AC044401 on 12/18/2015.
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My Second Angular 2 App</h1>'
+})
+export class AppComponent{ }
---
+++ app/boot.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by AC044401 on 12/18/2015.
+ */
+import {bootstrap} from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
