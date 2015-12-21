d67cf2e488b4f34d94c7e763a241ca2490029e1a
Adrian Cagaanan <adrian.cagaanan@cerner.com>
2015-12-21 08:51:38 -0600

    in the middle of changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,64 @@
  */
 import {Component} from 'angular2/core';
 
+interface Hero{
+    id: number;
+    name: string;
+}
 @Component({
     selector: 'my-app',
-    template: '<h1>My Second Angular 2 App</h1>'
+    template: `
+      <h1>{{title}}</h1>
+      <h2>My Heroes</h2>
+      <ul class="heroes">
+        <li *ngFor="#hero of heroes">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
+      </ul>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div><label>name: </label>{{hero.name}}</div>
+      <div>
+        <label>name: </label>
+        <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+      </div>
+      `,
+    styles:[`
+        .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em; }
+        .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+        .heroes li:hover {color: #369; background-color: #EEE; left: .2em; }
+        .heroes .badge {
+            font-size: small;
+            color: white;
+            padding: 0.1em 0.7em;
+            background-color: #369;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -1px;
+        }
+        .selected { background-color: #EEE; color: #369; }
+    `]
 })
-export class AppComponent{ }
+
+export class AppComponent{
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+    public heroes = HEROES;
+}
+
+var HEROES: Hero[] = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
